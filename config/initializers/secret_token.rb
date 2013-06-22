34e1024d865d83a39a685193cdccec4160538305
Pitbull <pitbull@lightningstrike.com>
2013-06-22 23:00:46 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoAppDell::Application.config.secret_token = 'ebee124d3decc0fab1fe5338b77e2bef6f5c6179672c15cd2020123635825c308cd74cb34dc89b8f48ceb0a4e6e085e87cc515e813cdfbc2f45e1b7165a6b472'
